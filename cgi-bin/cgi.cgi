fc42159ac280f9ad9c4180a31870e5f4f7ca2a52
Yash Patel <yash91066@gmail.com>
2024-10-03 12:06:28 -0600

    removed all files from sprint2

--- cgi-bin/cgi.cgi
+++
@@ -1,5 +0,0 @@
-#!"C:\Users\myadw\OneDrive\Desktop\php project\perl\bin\perl.exe"
-
-print "Content-Type: text/html\n\n";
-
-print "OK";
--- htdocs/OnlineBiddingSystem (1)/bidding/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	session_start();
-	$_SESSION['logged'] = 'guest';
-	header('location:home.php');
-?>
